90afacc8b3b0e299372403f34abba81fe30eab8e
yedidyaroy <76555587+yedidyaroy@users.noreply.github.com>
2021-06-21 09:51:49 +0530

    Add files via upload

---
+++ snow.js
@@ -0,0 +1,28 @@
+class Snow {
+    constructor(x, y) {
+        var options ={
+        restitution:0.4 
+         }
+        
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.image = loadImage("snow4.webp");
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        noStroke();
+      //  fill(this.image);
+        image(this.image, 0, 0);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
